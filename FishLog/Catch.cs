c130d4ff5aa976efc17b824e55f013c347a9c036
zswain2611 <A00126936@mycambrian.ca>
2026-04-07 11:02:06 -0400

    Complete Catch class implementation

--- FishLog/Catch.cs
+++ FishLog/Catch.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// FishLog - Ontario Fishing Trip Logger
+// Zach Swain
+// Catch.cs - Represents a single fish catch
+
+using System;
 
 namespace FishLog
 {
-    internal class Catch
+    public class Catch
     {
+        private string _species;
+        private double _lengthCm;
+        private double _weightKg;
+        private CatchStatus _status;
+        private DateTime _time;
+
+        public Catch(string species, double lengthCm, double weightKg, CatchStatus status)
+        {
+            _species = species;
+            _lengthCm = lengthCm;
+            _weightKg = weightKg;
+            _status = status;
+            _time = DateTime.Now;
+        }
+
+        public string GetSpecies() => _species;
+        public double GetLength() => _lengthCm;
+        public double GetWeight()=> _weightKg;
+        public CatchStatus GetStatus() => _status;
+        public string GetSummary() => $"{_species} {_lengthCm}cm {_status}";
+
+
     }
 }
