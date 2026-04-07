d04973367b0c6fc62f9afd0672cf34926c1b0d49
zswain2611 <A00126936@mycambrian.ca>
2026-04-07 15:58:41 -0400

    Add Angler class

---
+++ FishLog/Angler.cs
@@ -0,0 +1,33 @@
+﻿// FishLog - Ontario Fishing Trip Logger
+// Zach Swain
+// Angler.cs - Represents the user profile
+
+using System;
+using System.Collections.Generic;
+
+namespace FishLog
+{
+    public class Angler
+    {
+        private string _name;
+        private License _license;
+        private List<Trip> _trips;
+
+        public Angler(string name, License license)
+        {
+            _name = name;
+            _license = license;
+            _trips = new List<Trip>();
+        }
+
+        public Trip StartTrip(FMZone zone, string location)
+        {
+            Trip newTrip = new Trip(zone, location);
+            _trips.Add(newTrip);
+            return newTrip;
+        }
+
+        public string GetName() => _name;
+        public License GetLicense() => _license;
+    }
+}
