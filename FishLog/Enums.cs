186d0d8b5dfccffd23c6b4938716c6949fce5c9c
zswain2611 <A00126936@mycambrian.ca>
2026-04-07 09:26:19 -0400

    Add core enums

---
+++ FishLog/Enums.cs
@@ -0,0 +1,36 @@
+﻿// FishLog - Ontario Fishing Trip Logger
+// Zach Swain
+// Enums.cs - Core enumeration types for the application
+
+using System;
+
+namespace FishLog
+{
+
+    public enum LicenseType
+    {
+        Sport,
+        Conservation
+    }
+
+    public enum FMZone
+    {
+        FMZ10,
+        FMZ11
+    }
+
+    public enum CatchStatus
+    {
+        Kept,
+        Released
+    }
+
+    public enum ValidationResult
+    {
+        Legal,
+        OutOfSeason,
+        OverLimit,
+        IllegalSize,
+        MustRelease
+    }
+}
