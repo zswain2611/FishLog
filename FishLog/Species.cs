adf63cde54b5d09242090cf3ef99f3b34670fa65
zswain2611 <A00126936@mycambrian.ca>
2026-04-14 09:51:13 -0400

    Add Species abstract class

--- FishLog/Species.cs
+++ FishLog/Species.cs
@@ -1,29 +1,30 @@
-﻿using System;
+﻿// FishLog - Ontario Fishing Trip Logger
+// Zach Swain
+// Species.cs - Abstract base class for all fish species
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FishLog
 {
     public abstract class Species
     {
         protected string _commonName;
-        protected Dictionary<FMZone, RegulationRule> _rule;
+        protected Dictionary<FMZone, RegulationRule> _rules;
 
         public Species(string commonName)
         {
             _commonName = commonName;
-            _rule = new Dictionary<FMZone, RegulationRule>();
+            _rules = new Dictionary<FMZone, RegulationRule>();
         }
 
         // Abstract method: Each species implements its own validation logic
-        public abstract ValidationResult ValidateCatch(Catch fish, License liscense, FMZone zone, int KeptSoFar);
+        public abstract ValidationResult ValidateCatch(Catch fish, License license, FMZone zone, int keptSoFar);
 
         // Helper Methods
         public RegulationRule GetRule(FMZone zone)
         {
-            return _rule[zone];
+            return _rules[zone];
         }
 
         public bool IsInSeason(FMZone zone, DateTime date)
--- FishLog/YellowPerch.cs
+++ FishLog/YellowPerch.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// FishLog - Ontario Fishing Trip Logger
+// Zach Swain
+// YellowPerch.cs - Yellow Perch species with validation
+
+using System;
 
 namespace FishLog
 {
-    internal class YellowPerch
+    public class YellowPerch : Species
     {
+        public YellowPerch() : base("Yellow Perch")
+        {
+            // FMZ 10 Regulations
+            _rules[FMZone.FMZ10] = new RegulationRule(
+                seasonOpen: DateTime.MinValue,
+                seasonClose: DateTime.MinValue,
+                sportLimit: 50,
+                conservLimit: 25
+                // No size limit
+            );
+
+            // FMZ 11 Regulations
+            _rules[FMZone.FMZ11] = new RegulationRule(
+                seasonOpen: DateTime.MinValue,
+                seasonClose: DateTime.MinValue,
+                sportLimit: 50,
+                conservLimit: 25
+            );
+        }
+
+        public override ValidationResult ValidateCatch(Catch fish, License license, FMZone zone, int keptSoFar)
+        {
+            RegulationRule rule = GetRule(zone);
+
+            // Check if season is open
+            if (!IsInSeason(zone, DateTime.Now))
+            {
+                return ValidationResult.OutOfSeason;
+            }
+
+            // Check daily limit
+            int dailyLimit = rule.GetLimit(license.GetLicenseType());
+            if (keptSoFar >= dailyLimit)
+            {
+                return ValidationResult.OverLimit;
+            }
+
+            return ValidationResult.Legal;
+        }
     }
 }
